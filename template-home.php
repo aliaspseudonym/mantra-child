1cff3e587786ffecbc507cf96c2cc628709454f8
AliasPseudonym <alias13th@gmail.com>
2015-10-27 18:11:44 -0600

    Front page changes

--- template-home.php
+++ template-home.php
@@ -90,7 +90,7 @@ get_header(); ?>
 <div id="mc_embed_signup">
 <form action="//kolbetimes.us12.list-manage.com/subscribe/post?u=18821a9059b1c479c0607e9b3&amp;id=e3bc9fa822" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-	<h2>This is our last print issue!  Subscribe here to get future issues by email.</h2>
+	<h2>Kolbe Times is going digital!  We are no longer distributing print issues, but you can sign up here for your FREE Kolbe Times E-Magazine, emailed monthly. </h2>
 <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
 <div class="mc-field-group">
 	<label for="mce-LNAME">Name </label>
@@ -106,7 +106,7 @@ get_header(); ?>
 		<div class="response" id="mce-success-response" style="display:none"></div>
 	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_18821a9059b1c479c0607e9b3_e3bc9fa822" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div> <p>You may unsubscribe at any time.</p>
     </div>
 </form>
 </div>
@@ -122,7 +122,7 @@ get_header(); ?>
 			<div>
 				<br class="no-mobile" />
 				<h1>Book Reviews</h1>
-				<a href="<?php echo home_url('/'); ?>Book-review/"><h2>Books that Changed My Life</h2></a>
+				<a href="<?php echo home_url('/'); ?>Book-review/"><h2>Two New Treasures</h2></a>
 				<p>Kolbe Times Editor Laura Locke shares a couple of books she found delightful and inspiring.</p>
 				<p><a href="<?php echo home_url('/'); ?>category/written-word/book-reviews/">All Book Reviews</a></p>
 			</div>
@@ -143,7 +143,7 @@ get_header(); ?>
 				<br class="no-mobile" />
 				<h1>Guest Columnists</h1>
 				<a href="<?php echo home_url('/'); ?>jews-crucifixions-and-loving-the-difficult/"><h2>Loving the Difficult</h2></a>
-				<p>Grad Student Amanda Achtman reflects on the difficulty of life and art.</p>
+				<p>Graduate student Amanda Achtman reflects on two courageous artists who took great risks in expressing life, love and paradox.</p>
 				<p><a href="<?php echo home_url('/'); ?>category/guest-columnists/">All Guest Columnists</a></p>
 			</div>
 			<a href="<?php echo home_url('/'); ?>djews-crucifixions-and-loving-the-difficult/"><img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/05/GuestColumnistThumb.jpg"/></a>
@@ -165,7 +165,7 @@ get_header(); ?>
 				<br class="no-mobile" />
 				<h1>Film Reviews</h1>
 				<a href="<?php home_url('/'); ?>film-review-the-drop-box"><h2>The Drop Box</h2></a>
-				<p>Bill Locke reviews a documentary about a pastor who goes to great lengths to take in orphans no-one else wants.</p>
+				<p>Bill Locke reviews a new, award-winning documentary about a pastor and his wife, who take in babies that no one else wants.</p>
 				<p><a href="<?php echo home_url('/'); ?>category/film/film-reviews/">All Film Reviews</a></p>
 			</div>
 			<a href="<?php home_url('/'); ?>film-review-the-drop-box"><img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/05/FilmReviewsThumb.jpg"/></a>
@@ -175,7 +175,7 @@ get_header(); ?>
 				<br class="no-mobile" />
 				<h1>Editorials</h1>
 				<a href="<?php echo home_url('/'); ?>musings-from-the-editors-desk-fall-2015"><h2>Musings from the Editor's Desk</h2></a>
-				<p>This issue is all about connections, mysteries and hidden truths that lie underneath the literal level of meaning.</p>
+				<p>This issue is about “power beyond words” – the connections and hidden truths that are revealed through experience.</p>
 				<p><a href="<?php echo home_url('/'); ?>category/editorials">All Editorials</a></p>
 			</div>
 			<a href="<?php echo home_url('/'); ?>musings-from-the-editors-desk-fall-2015"><img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/05/musing.jpg"/></a>
@@ -188,19 +188,19 @@ get_header(); ?>
 		<div>
 			<a href="<?php echo home_url('/'); ?>the-vikas-india-housing-project/"><h1>VIKAS Housing Project</h1>
 			<img src="<?php echo home_url('/'); ?>wp-content/themes/mantra-child/images/vikasthumb.jpg" /></a>
-			<p>Nearly 70% of India’s 1.1 billion–plus population still lives in villages in mud huts without electricity, clean water or sanitation. A Calgary couple decided to do change that reality for 12 families.</p>
+			<p>Canadians partnering with a community in India, changing lives one family at a time.</p>
 		</div>
 		<div>
-			<a href="<?php echo home_url('/'); ?>photo-contest-results/"><h1>Photo Contest Results</h1>
+			<a href="<?php echo home_url('/'); ?>photo-contest-results/"><h1>Photo Contest Winners</h1>
 			<img src="<?php echo home_url('/'); ?>wp-content/themes/mantra-child/images/contestthumb.jpg" /></a>
-			<p>Take a look at the results of the photo contest from our 2015 Spring issue! </p>
+			<p>Take a look at the winners of our Spring 2015 Kolbe Times Photo Contest!</p>
 		</div>
 	</div> <!-- writers-column -->
 	<div class="writers-column-2">
 		<div>
 			<a href="<?php echo home_url('/'); ?>poetry-autumn/"><h1>Poetry: Autumn</h1>
 			<img src="<?php echo home_url('/'); ?>wp-content/themes/mantra-child/images/autumnthumb.jpg" /></a>
-			<p>By Rainer Maria Rilke (1875-1926)  (translation by Robert Bly) </p>
+			<p>By Rainer Maria Rilke (1875-1926), one of the most enigmatic and significant poets of the last century.</p>
 		</div>
 	</div> <!-- writers-column -->
 </div> <!-- writers -->
